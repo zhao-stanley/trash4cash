847f82cc1a4c4313ec890a6b4a84c05417a3df2d
Stanley <contact@stanleyweb.dev>
2022-05-10 17:59:44 +0000

    Note

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
+//find a way to add a big globe behind the hero text --> sort of like github
+
 const App = () => {
   return (
     <>
@@ -9,18 +11,23 @@ const App = () => {
       <div className="bg-gray-800 h-screen w-full flex flex-col items-center justify-center font-[Inter]">
         <div className="h-[80vh] items-center flex flex-col justify-center">
           <h1 className="text-[#60BF88] z-[1] font-bold text-7xl m-5 text-center break-words">
-            <span className="text-white">Save the </span>Earth<br />
-            <span className="text-white">Save </span>money<br />
+            <span className="text-white">Save the </span>Earth
+            <br />
+            <span className="text-white">Save </span>money
+            <br />
             <span className="text-white">It's a </span>win-win.
           </h1>
-          <Link className="bg-[#60BF88] opacity-[1] text-white p-4 text-2xl font-medium rounded-xl transition-[3s] hover:bg-[#60bf88be] mt-8" to="/browse">Browse available stores</Link>
+          <Link
+            className="bg-[#60BF88] opacity-[1] text-white p-4 text-2xl font-medium rounded-xl transition-[3s] hover:bg-[#60bf88be] mt-8"
+            to="/browse"
+          >
+            Browse available stores
+          </Link>
         </div>
-        <div className="h-[20vh]">
-
-        </div>
+        <div className="h-[20vh]"></div>
       </div>
     </>
   );
-}
+};
 
 export default App;
